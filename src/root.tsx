4f82372ca1f5116ad063f20e60db7746adbe6372
Nguyen Duy <wuuyi123@gmail.com>
2023-01-25 20:27:15 +0700

    Add favicon

--- src/root.tsx
+++ src/root.tsx
@@ -11,6 +11,7 @@ import {
   Routes,
   Scripts,
   Title,
+  Link
 } from 'solid-start';
 
 import Footer from '~/components/Footer';
@@ -21,10 +22,11 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>League Loader</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta name="theme-color" content="#0A323C" />
+        <Title>League Loader</Title>
+        <Link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
       <Body>
         <Suspense>
