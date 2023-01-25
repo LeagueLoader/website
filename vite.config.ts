447e90bf1a2073e9d5de9eefadff49e0f2ccdc48
Nguyen Duy <wuuyi123@gmail.com>
2023-01-25 20:11:49 +0700

    Add deno build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
-import solid from "solid-start/vite";
-import { defineConfig } from "vite";
+import solid from 'solid-start/vite';
+import { defineConfig } from 'vite';
+
+// @ts-ignore
+import deno from 'solid-start-deno';
+// @ts-ignore
+import node from 'solid-start-node';
+
+const useAdapter = process.argv.includes('--deno') ? deno : node;
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid({
+      adapter: useAdapter()
+    })
+  ],
 });
